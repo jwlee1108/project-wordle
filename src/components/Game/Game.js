07149ed6436c40dc65d9184d4538284e1fe10942
jinwoo-lee <jw.lee@nhn.com>
2023-10-17 07:37:56 +0900

    module 1, 2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,50 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+  const [guessList, setGuessList] = React.useState([]);
+
+  return (
+    <>
+      <form
+        className="guess-input-wrapper"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          const reValidGuess = /^[A-Z]{5}$/;
+          const isValidGuess = reValidGuess.test(guess);
+
+          if (isValidGuess) {
+            const currGuess = { id: crypto.randomUUID(), guess };
+            const nextGuessList = [...guessList];
+            nextGuessList.push(currGuess);
+            console.log(currGuess);
+
+            setGuessList(nextGuessList);
+            setGuess("");
+          }
+        }}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          value={guess}
+          maxLength={5}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        />
+      </form>
+      <div className="guess-results">
+        {guessList.map((guess) => (
+          <p className="guess" key={guess.id}>
+            {guess.guess}
+          </p>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Game;
